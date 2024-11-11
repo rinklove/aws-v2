e260930ad42d33f94c78372e581a00a973b4dccb
junho <>
2024-11-11 11:30:04 +0900

    fix: 테스트 수정

--- src/test/java/site/metacoding/awsv2/HelloControllerTest.java
+++ src/test/java/site/metacoding/awsv2/HelloControllerTest.java
@@ -1,13 +1,16 @@
 package site.metacoding.awsv2;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
@@ -17,7 +20,7 @@ public class HelloControllerTest {
 
     @Test
     public void hello_테스트(){
-        ResponseEntity<String> response = restTemplate.getForEntity("/aws/v2", String.class);       
-        assertEquals("<h1>aws v2</h1>", response.getBody());
+        ResponseEntity<String> response = restTemplate.getForEntity("/aws/v2", String.class);
+        assertTrue(Objects.requireNonNull(response.getBody()).startsWith("<h1>aws v2 number"));
     }
 }
